af2c56cdb2d7e66c109cdb03dd97d31d0e6bc5be
Jim Vargas <jimball1@hotmail.com>
2017-08-08 17:13:11 -0700

    List will spit out alphabetically (bonus mission)

--- src/org/launchcode/techjobs/console/JobData.java
+++ src/org/launchcode/techjobs/console/JobData.java
@@ -8,10 +8,7 @@ import org.apache.commons.csv.CSVRecord;
 import java.io.FileReader;
 import java.io.IOException;
 import java.io.Reader;
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 
 /**
  * Created by LaunchCode
@@ -44,6 +41,7 @@ public class JobData {
                 values.add(aValue);
             }
         }
+        values.sort(String::compareToIgnoreCase);
 
         return values;
     }
